17ff3622a80bb90e07fabe59ae2b6f05302d7720
Ricardo Bonin <ricardo.bonin@imobles.com>
2023-08-30 16:40:46 -0300

    feat: create delete categories

---
+++ remove-categories.js
@@ -0,0 +1,25 @@
+// Start BD sudo /opt/lampp/manager-linux-x64.run
+
+const mysql = require('mysql2/promise')
+
+const run = async () => {
+  try {
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      database: 'cat-products',
+    })
+
+    try {
+      const [results, fields] = await connection.query('delete from categories where id = ? limit 1', [2])
+
+      console.log('Categories => ', results)
+    } catch (error) {
+      console.log(error)
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+run()
