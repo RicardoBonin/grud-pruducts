6d3e2f8d0957fedb86e6391752ee3c6b078452e6
Ricardo Bonin <ricardo.bonin@imobles.com>
2024-01-10 10:38:25 -0300

    feat: apply pool sql

--- db.js
+++ db.js
@@ -1,7 +1,16 @@
 const mysql = require('mysql2/promise')
 
-module.exports = mysql.createConnection({
+// module.exports = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'cat-products',
+// })
+
+module.exports = mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'cat-products',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 })
